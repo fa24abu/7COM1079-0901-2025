2cd592e4e91e6d6de7de23847308e361bf6ce914
fa24abu <fa24abu@herts.ac.uk>
2025-12-02 22:44:14 +0000

    Modified load data & Added clean column names

--- analysis.R
+++ analysis.R
@@ -1,7 +1,9 @@
-# Install both libraries in one command
-install.packages(c("readr", "readxl"))
 library(readr)
 library(readxl)
 
 # Load data using readr
 data <- read_csv("CanabisBenefits.csv")
+
+# Clean column names
+colnames(data)[4] <- "evidence_score"
+colnames(data)[5] <- "popular_interest"
